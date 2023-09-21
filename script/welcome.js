3d9674a965774861611b2bfb8a9b1c24c4b57a3e
MoEzwawi <mezwawi96@gmail.com>
2023-09-21 22:33:55 +0200

    small upgrades

--- script/welcome.js
+++ script/welcome.js
@@ -1,24 +1,24 @@
 const check = function () {
 
-    const checked = document.getElementById ('mySpan')
-   
-    if (checked.innerText ==='' ) {
-    checked.innerText = "✔"
-    
+  const checked = document.getElementById ('mySpan')
+ 
+  if (checked.innerText ==='' ) {
+  checked.innerText = "✔"
+  
 } else {checked.innerText=''} }
 
 
 
 const goToBenchmark = function()
- { const checked = document.getElementById ('mySpan')
-   const number=document.getElementById("num")
-   const select = document.getElementById('diff')
-   if (!(number.value < 5 || number.value > 10) && checked.innerText !=='' && select.value !== "not_selected"){
-      window.location.href="index.html"
-    }
-   let questionsNumber = JSON.parse(number.value)
-   localStorage.setItem('number',questionsNumber)
-   
-   let testDifficulty = JSON.stringify(select.value)
-   localStorage.setItem('difficulty',testDifficulty)
-	 }
+{ const checked = document.getElementById ('mySpan')
+ const number=document.getElementById("num")
+ const select = document.getElementById('diff')
+ if (!(number.value < 5 || number.value > 10) && checked.innerText !=='' && select.value !== "not_selected"){
+    window.location.href="index.html"
+  }
+ let questionsNumber = JSON.parse(number.value)
+ localStorage.setItem('number',questionsNumber)
+ 
+ let testDifficulty = select.value
+ localStorage.setItem('difficulty',testDifficulty)
+ }
