da46adb4aa6d464899087a175689f0bd6371d2d9
Bruno Capobianco <bruno.capobianco@outlook.it>
2023-09-21 14:51:56 +0200

    some bug fixed

--- script/javascript.js
+++ script/javascript.js
@@ -1,3 +1,103 @@
+const questions = [
+	{	
+		category: "Science: Computers",
+		type: "multiple",
+		difficulty: "easy",
+		question: "What does CPU stand for?",
+		correct_answer: "Central Processing Unit",
+		incorrect_answers: [
+			"Central Process Unit",
+			"Computer Personal Unit",
+			"Central Processor Unit",
+		],
+	},
+	{
+		category: "Science: Computers",
+		type: "multiple",
+		difficulty: "hard",
+		question:
+			"In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
+		correct_answer: "Final",
+		incorrect_answers: ["Static", "Private", "Public"],
+	},
+	{
+		category: "Science: Computers",
+		type: "boolean",
+		difficulty: "easy",
+		question: "The logo for Snapchat is a Bell.",
+		correct_answer: "False",
+		incorrect_answers: ["True"],
+	},
+	{
+		category: "Science: Computers",
+		type: "boolean",
+		difficulty: "easy",
+		question:
+			"Pointers were not used in the original C programming language; they were added later on in C++.",
+		correct_answer: "False",
+		incorrect_answers: ["True"],
+	},
+	{
+		category: "Science: Computers",
+		type: "multiple",
+		difficulty: "easy",
+		question:
+			"What is the most preferred image format used for logos in the Wikimedia database?",
+		correct_answer: ".svg",
+		incorrect_answers: [".png", ".jpeg", ".gif"],
+	},
+	{
+		category: "Science: Computers",
+		type: "multiple",
+		difficulty: "easy",
+		question: "In web design, what does CSS stand for?",
+		correct_answer: "Cascading Style Sheet",
+		incorrect_answers: [
+			"Counter Strike: Source",
+			"Corrective Style Sheet",
+			"Computer Style Sheet",
+		],
+	},
+	{
+		category: "Science: Computers",
+		type: "multiple",
+		difficulty: "normal",
+		question:
+			"What is the code name for the mobile operating system Android 7.0?",
+		correct_answer: "Nougat",
+		incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
+	},
+	{
+		category: "Science: Computers",
+		type: "multiple",
+		difficulty: "easy",
+		question: "On Twitter, what is the character limit for a Tweet?",
+		correct_answer: "140",
+		incorrect_answers: ["120", "160", "100"],
+	},
+	{
+		category: "Science: Computers",
+		type: "boolean",
+		difficulty: "easy",
+		question: "Linux was first created as an alternative to Windows XP.",
+		correct_answer: "False",
+		incorrect_answers: ["True"],
+	},
+	{
+		category: "Science: Computers",
+		type: "multiple",
+		difficulty: "normal",
+		question:
+			"Which programming language shares its name with an island in Indonesia?",
+		correct_answer: "Java",
+		incorrect_answers: ["Python", "C", "Jakarta"],
+	},
+	
+	
+	
+];
+
+
 const semicircles = document.querySelectorAll(".semicircle");
 const timer = document.querySelector(".timer");
 
@@ -64,7 +164,17 @@ const startquiz=function()
 
 let hr = 0;
 let min = 0;
-let sec = 30; // set timer
+let sec = 0; // set timer
+switch (questions[0].difficulty)
+{
+	case "easy": sec=30;
+		break;
+	case "normal": sec=60;
+		break;
+	case "hard": sec=90;
+		break;
+}
+
 
 let hours = hr * 3600000;
 let minutes = min * 60000;
@@ -140,11 +250,28 @@ function resetTimer() {
 	let hr = 0;
 	let min = 0;
 	let sec = 30; //CAMBIARE QUI
+
+	if(i<questions.length)
+	{
+		switch (questions[i+1].difficulty)
+		{
+			case "easy": sec=30;
+				break;
+			case "normal": sec=60;
+				break;
+			case "hard": sec=90;
+				break;
+		}
+	}
+
+
 	let hours = hr * 3600000;
 	let minutes = min * 60000;
 	let seconds = sec * 1000;
 	let setTime = hours + minutes + seconds;
 	futureTime = Date.now() + setTime; // Aggiorna futureTime
+
+
 
 	// rimettiamo le funzioni del tempo
 	semicircles[2].style.transform = `rotate(360deg)`;
@@ -155,101 +282,7 @@ function resetTimer() {
 }
 
 //result score
-const questions = [
-	{	
-		category: "Science: Computers",
-		type: "multiple",
-		difficulty: "easy",
-		question: "What does CPU stand for?",
-		correct_answer: "Central Processing Unit",
-		incorrect_answers: [
-			"Central Process Unit",
-			"Computer Personal Unit",
-			"Central Processor Unit",
-		],
-	},
-	{
-		category: "Science: Computers",
-		type: "multiple",
-		difficulty: "easy",
-		question:
-			"In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
-		correct_answer: "Final",
-		incorrect_answers: ["Static", "Private", "Public"],
-	},
-	{
-		category: "Science: Computers",
-		type: "boolean",
-		difficulty: "easy",
-		question: "The logo for Snapchat is a Bell.",
-		correct_answer: "False",
-		incorrect_answers: ["True"],
-	},
-	{
-		category: "Science: Computers",
-		type: "boolean",
-		difficulty: "easy",
-		question:
-			"Pointers were not used in the original C programming language; they were added later on in C++.",
-		correct_answer: "False",
-		incorrect_answers: ["True"],
-	},
-	{
-		category: "Science: Computers",
-		type: "multiple",
-		difficulty: "easy",
-		question:
-			"What is the most preferred image format used for logos in the Wikimedia database?",
-		correct_answer: ".svg",
-		incorrect_answers: [".png", ".jpeg", ".gif"],
-	},
-	{
-		category: "Science: Computers",
-		type: "multiple",
-		difficulty: "easy",
-		question: "In web design, what does CSS stand for?",
-		correct_answer: "Cascading Style Sheet",
-		incorrect_answers: [
-			"Counter Strike: Source",
-			"Corrective Style Sheet",
-			"Computer Style Sheet",
-		],
-	},
-	{
-		category: "Science: Computers",
-		type: "multiple",
-		difficulty: "easy",
-		question:
-			"What is the code name for the mobile operating system Android 7.0?",
-		correct_answer: "Nougat",
-		incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
-	},
-	{
-		category: "Science: Computers",
-		type: "multiple",
-		difficulty: "easy",
-		question: "On Twitter, what is the character limit for a Tweet?",
-		correct_answer: "140",
-		incorrect_answers: ["120", "160", "100"],
-	},
-	{
-		category: "Science: Computers",
-		type: "boolean",
-		difficulty: "easy",
-		question: "Linux was first created as an alternative to Windows XP.",
-		correct_answer: "False",
-		incorrect_answers: ["True"],
-	},
-	{
-		category: "Science: Computers",
-		type: "multiple",
-		difficulty: "easy",
-		question:
-			"Which programming language shares its name with an island in Indonesia?",
-		correct_answer: "Java",
-		incorrect_answers: ["Python", "C", "Jakarta"],
-	},
-];
+
 const svuotatutto = function (e) 
 	{
 		const everytab = document.getElementsByClassName("cellSelected");
@@ -318,3 +351,4 @@ console.log(i);
 
 // console.log(questions);
 //const confirm=document.createElement("div");
+
