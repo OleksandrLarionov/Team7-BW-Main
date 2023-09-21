aaeee7cc75507e93cd696b56b438d14bbd412eb9
Bruno Capobianco <bruno.capobianco@outlook.it>
2023-09-21 10:39:00 +0200

    some bugs fixed

--- script/javascript.js
+++ script/javascript.js
@@ -1,5 +1,65 @@
 const semicircles = document.querySelectorAll(".semicircle");
 const timer = document.querySelector(".timer");
+
+const startquiz=function()
+{
+	if (i < questions.length) {
+		question.innerText = questions[i].question;
+
+		if (questions[i].type === "multiple") 
+		{
+			const randomNumArr = [0, 1, 2, 3];
+			const randomNum = function () {
+				const i = Math.floor(Math.random() * randomNumArr.length);
+				const randomNumber = randomNumArr.splice(i, 1)[0];
+				return randomNumber;
+			};
+
+			answ[0].innerText = questions[i].correct_answer;
+			answ[1].innerText = questions[i].incorrect_answers[0];
+			answ[2].innerText = questions[i].incorrect_answers[1];
+			answ[3].innerText = questions[i].incorrect_answers[2];
+			answ[2].style.display = "inline-block";
+			answ[3].style.display = "inline-block";
+			answ[0].classList.add("answer-box");
+			answ[1].classList.add("answer-box");
+			answ[2].classList.add("answer-box");
+			answ[3].classList.add("answer-box");
+			answer.appendChild(answ[randomNum()]);
+			answer.appendChild(answ[randomNum()]);
+			answer.appendChild(answ[randomNum()]);
+			answer.appendChild(answ[randomNum()]);
+			
+		} else if (questions[i].type === "boolean") {
+			const randomNumArr = [0, 1, 2, 3];
+			const randomNum = function () {
+				const i = Math.floor(Math.random() * randomNumArr.length);
+				const randomNumber = randomNumArr.splice(i, 1)[0];
+				return randomNumber;
+			};
+
+			answ[0].innerText = questions[i].correct_answer;
+			answ[1].innerText = questions[i].incorrect_answers[0];
+			answ[2].style.display = "none";
+			answ[3].style.display = "none";
+			answ[0].classList.add("answer-box");
+			answ[1].classList.add("answer-box");
+			answ[2].classList.add("answer-box");
+			answ[3].classList.add("answer-box");
+			answer.appendChild(answ[randomNum()]);
+			answer.appendChild(answ[randomNum()]);
+			answer.appendChild(answ[randomNum()]);
+			answer.appendChild(answ[randomNum()]);
+		}
+	} else {
+		//alert("test finito, totale punteggio: " + score);
+		resetTimer();
+		let score_serialized = JSON.parse(score);
+		localStorage.setItem("score", score_serialized);
+		window.location.href="result-score.html"
+	}
+}
+
 //input
 
 let hr = 0;
@@ -30,6 +90,7 @@ const moveCircles = () => {
 		semicircles[2].style.transform = `rotate(180deg)`;
 		semicircles[2].style.backgroundColor = "rgb(196, 164, 200)";
 	}
+	
 };
 
 const countDownTime = () => {
@@ -50,8 +111,11 @@ const countDownTime = () => {
 		semicircles[1].style.display = "none";
 		semicircles[2].style.display = "none";
 		timer.innerHTML = `<div class='colon'>0</div>`;
+		i++;
+		counter.innerText = i + 1;
+		resetTimer();
+		startquiz();
 	}
-
 	if (secs > 19 && secs < 100) {
 		const timerPosition = (document.querySelector(".timer").style.left =
 			"32px");
@@ -63,9 +127,7 @@ const countDownTime = () => {
 			"47px");
 	}
 };
-
 let timeLoop = setInterval(countDownTime); // continua a richiamare la mia funzione del tempo e aggiorna il timer
-
 function resetTimer() {
 	semicircles[0].style.display = "block";
 	semicircles[1].style.display = "block";
@@ -73,7 +135,7 @@ function resetTimer() {
 
 	let hr = 0;
 	let min = 0;
-	let sec = 30;
+	let sec = 30; //CAMBIARE QUI
 	let hours = hr * 3600000;
 	let minutes = min * 60000;
 	let seconds = sec * 1000;
@@ -89,9 +151,8 @@ function resetTimer() {
 }
 
 //result score
-
 const questions = [
-	{
+	{	
 		category: "Science: Computers",
 		type: "multiple",
 		difficulty: "easy",
@@ -185,16 +246,16 @@ const questions = [
 		incorrect_answers: ["Python", "C", "Jakarta"],
 	},
 ];
-
-const svuotatutto = function (e) {
-	const everytab = document.getElementsByClassName("cellSelected");
-	const everytab1 = Array.from(everytab);
-	for (let i = 0; i < everytab1.length; i++) {
+const svuotatutto = function (e) 
+	{
+		const everytab = document.getElementsByClassName("cellSelected");
+		const everytab1 = Array.from(everytab);
+		for (let i = 0; i < everytab1.length; i++) 
+	{
 		everytab1[i].classList.remove("cellSelected");
 	}
 	confirm.style.display = "none";
 };
-
 const question = document.getElementById("question");
 const answer = document.getElementById("answers");
 const answ = [];
@@ -214,71 +275,21 @@ confirm.addEventListener("click", function () {
 	let risp = document.getElementsByClassName("cellSelected");
 	let risp2 = Array.from(risp);
 	console.log(risp2[0].innerText);
-	if (risp2[0].innerText === questions[i].correct_answer) {
+	if (risp2[0].innerText === questions[i].correct_answer) 
+	{
 		score += 1;
 		console.log(score);
 	}
 	i++;
 	counter.innerText = i + 1;
-
 	svuotatutto();
-
-	if (i < questions.length) {
-		question.innerText = questions[i].question;
-
-		if (questions[i].type === "multiple") {
-			const randomNumArr = [0, 1, 2, 3];
-			const randomNum = function () {
-				const i = Math.floor(Math.random() * randomNumArr.length);
-				const randomNumber = randomNumArr.splice(i, 1)[0];
-				return randomNumber;
-			};
-
-			answ[0].innerText = questions[i].correct_answer;
-			answ[1].innerText = questions[i].incorrect_answers[0];
-			answ[2].innerText = questions[i].incorrect_answers[1];
-			answ[3].innerText = questions[i].incorrect_answers[2];
-			answ[2].style.display = "inline-block";
-			answ[3].style.display = "inline-block";
-			answ[0].classList.add("answer-box");
-			answ[1].classList.add("answer-box");
-			answ[2].classList.add("answer-box");
-			answ[3].classList.add("answer-box");
-			answer.appendChild(answ[randomNum()]);
-			answer.appendChild(answ[randomNum()]);
-			answer.appendChild(answ[randomNum()]);
-			answer.appendChild(answ[randomNum()]);
-		} else if (questions[i].type === "boolean") {
-			const randomNumArr = [0, 1, 2, 3];
-			const randomNum = function () {
-				const i = Math.floor(Math.random() * randomNumArr.length);
-				const randomNumber = randomNumArr.splice(i, 1)[0];
-				return randomNumber;
-			};
-
-			answ[0].innerText = questions[i].correct_answer;
-			answ[1].innerText = questions[i].incorrect_answers[0];
-			answ[2].style.display = "none";
-			answ[3].style.display = "none";
-			answ[0].classList.add("answer-box");
-			answ[1].classList.add("answer-box");
-			answ[2].classList.add("answer-box");
-			answ[3].classList.add("answer-box");
-			answer.appendChild(answ[randomNum()]);
-			answer.appendChild(answ[randomNum()]);
-			answer.appendChild(answ[randomNum()]);
-			answer.appendChild(answ[randomNum()]);
-		}
-	} else {
-		alert("test finito, totale punteggio: " + score);
-		//INSERIRE QUI LA VARIABILE LOCALSTORAGE
-		let score_serialized = JSON.parse(score);
-		localStorage.setItem("score", score_serialized);
-	}
+	startquiz();
 });
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < 4; i++) 
+{
 	answ[i] = document.createElement("div");
-	answ[i].addEventListener("click", function (e) {
+	answ[i].addEventListener("click", function (e) 
+	{
 		svuotatutto(e);
 		answ[i].classList.add("cellSelected");
 		confirm.style.display = "inline-block";
@@ -291,22 +302,8 @@ const r = function () {
 	let num = random.splice(i, 1)[0];
 	return num;
 };
+startquiz();
 
-question.innerText = questions[i].question;
-answ[0].innerText = questions[i].correct_answer;
-answ[1].innerText = questions[i].incorrect_answers[0];
-answ[2].innerText = questions[i].incorrect_answers[1];
-answ[3].innerText = questions[i].incorrect_answers[2];
-answ[0].classList.add("answer-box");
-answ[1].classList.add("answer-box");
-answ[2].classList.add("answer-box");
-answ[3].classList.add("answer-box");
-answer.appendChild(answ[r()]);
-answer.appendChild(answ[r()]);
-answer.appendChild(answ[r()]);
-answer.appendChild(answ[r()]);
-console.log(i);
-console.log(questions[i].question);
 
 // const footer = document.getElementsByTagName('footer')[0]
 
